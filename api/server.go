8ada0e775e82dba0d2d339f1be6bb9d0045e2b46
tatekicct <tatekicct@gmail.com>
2025-07-13 09:39:48 +0900

    Add: ユーザーのパスワードをハッシュ化

--- api/server.go
+++ api/server.go
@@ -40,6 +40,8 @@ func errorResponse(err error) gin.H {
 func (server *Server) setupRouter() {
 	router := gin.Default()
 
+	router.POST("/users", server.createUser)
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
